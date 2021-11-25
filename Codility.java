c8231772c6fcc22474e19cd3954aa42c3c0ce4f7
Merfuerkin <95003245+Merfuerkin@users.noreply.github.com>
2021-11-25 09:38:30 -0500

    Codility
    Homework week 4

---
+++ Codility.java
@@ -0,0 +1,42 @@
+package Week04HomeWork;
+
+public class CodilityTester {
+
+	public static void main(String[] args) {
+
+		String str1 = "Codility";
+		String str2 = "Test";
+		String str3 = "Coder";
+
+		String output = " ";
+		for (int i = 1; i < 31; i++) {
+			if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+				output = str1 + str2 + str3;
+			}
+			if (i % 2 == 0 && i % 3 == 0) {
+				output = str1 + str2;
+			}
+			if (i % 3 == 0 && i % 5 == 0)
+
+			{
+				output = str2 + str3;
+			}
+			if (i % 3 == 0) {
+				output = str2;
+			}
+			if (i % 5 == 0) {
+				output = str3;
+			}
+			if (i % 2 == 0) {
+				output = str1;
+			}
+			if (output != " ") {
+				System.out.println(output);
+			} else {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
